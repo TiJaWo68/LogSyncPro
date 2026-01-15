922e0a8eb7b1d48ef5b783928ea2f8bc16c4e08b
t68 <till.woitendorf@gmail.com>
2026-01-15 18:48:40 +0100

    Refactor AboutDialog to load libraries from licenses.xml

--- src/main/java/de/in/lsp/ui/dialog/AboutDialog.java
+++ src/main/java/de/in/lsp/ui/dialog/AboutDialog.java
@@ -83,7 +83,8 @@ public class AboutDialog extends JDialog {
 		}
 
 		// Close on Escape
-		getRootPane().registerKeyboardAction(e -> dispose(), javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0),
+		getRootPane().registerKeyboardAction(e -> dispose(),
+				javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0),
 				JComponent.WHEN_IN_FOCUSED_WINDOW);
 	}
 
@@ -185,14 +186,7 @@ public class AboutDialog extends JDialog {
 		JPanel listPanel = new JPanel();
 		listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
 
-		addLib(listPanel, "FlatLaf", "3.7", "Apache 2.0", "https://www.formdev.com/flatlaf/");
-		addLib(listPanel, "MigLayout", "11.3", "BSD 3-Clause", "http://www.miglayout.com");
-		addLib(listPanel, "Apache Commons Compress", "1.26.1", "Apache 2.0", "https://commons.apache.org/proper/commons-compress/");
-		addLib(listPanel, "XZ for Java", "1.9", "Public Domain", "https://tukaani.org/xz/java.html");
-		addLib(listPanel, "Apache Mina SSHD", "2.12.0", "Apache 2.0", "https://mina.apache.org/sshd-project/");
-		addLib(listPanel, "Cuberact Swing Layout", "1.1.1", "Unknown", "https://cuberact.org");
-		addLib(listPanel, "Logback Classic", "1.5.3", "EPL 1.0 / LGPL 2.1", "https://logback.qos.ch/");
-		addLib(listPanel, "SLF4J", "For Logback", "MIT", "https://www.slf4j.org/");
+		loadLibrariesFromXml(listPanel);
 
 		JScrollPane scrollPane = new JScrollPane(listPanel);
 		scrollPane.setBorder(null);
@@ -202,12 +196,63 @@ public class AboutDialog extends JDialog {
 		return panel;
 	}
 
+	private void loadLibrariesFromXml(JPanel listPanel) {
+		try (java.io.InputStream is = getClass().getResourceAsStream("/licenses.xml")) {
+			if (is == null) {
+				listPanel.add(new JLabel("Keine Lizenzinformationen gefunden (licenses.xml fehlt)."));
+				return;
+			}
+
+			javax.xml.parsers.DocumentBuilderFactory factory = javax.xml.parsers.DocumentBuilderFactory.newInstance();
+			javax.xml.parsers.DocumentBuilder builder = factory.newDocumentBuilder();
+			org.w3c.dom.Document doc = builder.parse(is);
+
+			org.w3c.dom.NodeList dependencies = doc.getElementsByTagName("dependency");
+
+			for (int i = 0; i < dependencies.getLength(); i++) {
+				org.w3c.dom.Element dep = (org.w3c.dom.Element) dependencies.item(i);
+				String artifactId = getTagValue("artifactId", dep);
+				String version = getTagValue("version", dep);
+
+				String name = artifactId; // Default to artifactId
+				String licenseName = "Unknown";
+				String url = "";
+
+				// Parsing licenses
+				org.w3c.dom.NodeList licenses = dep.getElementsByTagName("license");
+				if (licenses.getLength() > 0) {
+					org.w3c.dom.Element license = (org.w3c.dom.Element) licenses.item(0);
+					licenseName = getTagValue("name", license);
+					url = getTagValue("url", license);
+				}
+
+				addLib(listPanel, name, version, licenseName, url);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			listPanel.add(new JLabel("Fehler beim Laden der Lizenzinformationen."));
+		}
+	}
+
+	private String getTagValue(String tag, org.w3c.dom.Element element) {
+		org.w3c.dom.NodeList nodeList = element.getElementsByTagName(tag);
+		if (nodeList != null && nodeList.getLength() > 0) {
+			org.w3c.dom.Node node = nodeList.item(0).getFirstChild();
+			if (node != null) {
+				return node.getNodeValue();
+			}
+		}
+		return "";
+	}
+
 	private void addLib(JPanel container, String name, String version, String license, String url) {
 		JPanel item = new JPanel(new BorderLayout());
-		item.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(60, 60, 60)),
-				BorderFactory.createEmptyBorder(8, 0, 8, 0)));
+		item.setBorder(
+				BorderFactory.createCompoundBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(60, 60, 60)),
+						BorderFactory.createEmptyBorder(8, 0, 8, 0)));
 
-		JLabel nameLabel = new JLabel("<html><b>" + name + "</b> <span style='color:gray'>(" + version + ")</span></html>");
+		JLabel nameLabel = new JLabel(
+				"<html><b>" + name + "</b> <span style='color:gray'>(" + version + ")</span></html>");
 		JLabel licenseLabel = new JLabel("License: " + license);
 		licenseLabel.setForeground(Color.GRAY);
 
