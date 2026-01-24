a01aa0eed8bb0fc0dfec9a31a23d4848c396babb
t68 <till.woitendorf@gmail.com>
2026-01-24 14:44:29 +0100

    Adjust column widths in K8sPodSelectionDialog: Fixed checkbox column width, increased dialog width to 800px, and implemented 20/40/40 distribution for Namespace/Pod/Container columns using constants.

--- src/main/java/de/in/lsp/ui/K8sPodSelectionDialog.java
+++ src/main/java/de/in/lsp/ui/K8sPodSelectionDialog.java
@@ -24,95 +24,114 @@ import de.in.lsp.util.LspLogger;
  */
 public class K8sPodSelectionDialog extends JDialog {
 
-    private final JTable table;
-    private final DefaultTableModel tableModel;
-    private final List<SelectedContainer> selectedContainers = new ArrayList<>();
-    private boolean confirmed = false;
+	public static final int DIALOG_WIDTH = 800;
+	public static final int DIALOG_HEIGHT = 600;
+	public static final int CHECKBOX_COL_WIDTH = 25;
 
-    public K8sPodSelectionDialog(Frame parent, List<SshK8sService.K8sNamespace> data) {
-        super(parent, "Select K8s Containers", true);
-        setLayout(new BorderLayout());
+	private final JTable table;
+	private final DefaultTableModel tableModel;
+	private final List<SelectedContainer> selectedContainers = new ArrayList<>();
+	private boolean confirmed = false;
 
-        String[] columnNames = { "Select", "Namespace", "Pod", "Container" };
-        tableModel = new DefaultTableModel(columnNames, 0) {
-            @Override
-            public Class<?> getColumnClass(int columnIndex) {
-                return columnIndex == 0 ? Boolean.class : String.class;
-            }
+	public K8sPodSelectionDialog(Frame parent, List<SshK8sService.K8sNamespace> data) {
+		super(parent, "Select K8s Containers", true);
+		setLayout(new BorderLayout());
 
-            @Override
-            public boolean isCellEditable(int row, int column) {
-                return column == 0;
-            }
-        };
+		String[] columnNames = { "Select", "Namespace", "Pod", "Container" };
+		tableModel = new DefaultTableModel(columnNames, 0) {
+			@Override
+			public Class<?> getColumnClass(int columnIndex) {
+				return columnIndex == 0 ? Boolean.class : String.class;
+			}
 
-        for (SshK8sService.K8sNamespace ns : data) {
-            for (SshK8sService.K8sPod pod : ns.getPods()) {
-                for (String container : pod.getContainers()) {
-                    tableModel.addRow(new Object[] { false, ns.getName(), pod.getName(), container });
-                }
-            }
-        }
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return column == 0;
+			}
+		};
 
-        table = new JTable(tableModel);
-        table.setFillsViewportHeight(true);
-        javax.swing.table.TableRowSorter<DefaultTableModel> sorter = new javax.swing.table.TableRowSorter<>(tableModel);
-        table.setRowSorter(sorter);
+		for (SshK8sService.K8sNamespace ns : data) {
+			for (SshK8sService.K8sPod pod : ns.getPods()) {
+				for (String container : pod.getContainers()) {
+					tableModel.addRow(new Object[] { false, ns.getName(), pod.getName(), container });
+				}
+			}
+		}
 
-        K8sPodSelectionFilterPanel filterPanel = new K8sPodSelectionFilterPanel(table, sorter);
-        FilteredTablePanel filteredTablePanel = new FilteredTablePanel(table, filterPanel);
+		table = new JTable(tableModel);
+		table.setFillsViewportHeight(true);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		javax.swing.table.TableRowSorter<DefaultTableModel> sorter = new javax.swing.table.TableRowSorter<>(tableModel);
+		table.setRowSorter(sorter);
 
-        add(filteredTablePanel, BorderLayout.CENTER);
+		// Fixed width for "Select" column
+		table.getColumnModel().getColumn(0).setMaxWidth(CHECKBOX_COL_WIDTH);
+		table.getColumnModel().getColumn(0).setMinWidth(CHECKBOX_COL_WIDTH);
+		table.getColumnModel().getColumn(0).setPreferredWidth(CHECKBOX_COL_WIDTH);
+		table.getColumnModel().getColumn(0).setResizable(false);
 
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        JButton okButton = new JButton("Import Selected");
-        okButton.addActionListener(e -> {
-            collectSelected();
-            LspLogger.info(
-                    "User confirmed selection of " + selectedContainers.size() + " containers from selection dialog.");
-            confirmed = true;
-            dispose();
-        });
-        JButton cancelButton = new JButton("Cancel");
-        cancelButton.addActionListener(e -> dispose());
+		// Distribute remaining space: 20% Namespace, 40% Pod, 40% Container Dialog width is 600. Select is 25. Remaining is 575. 20% of 575
+		// = 115 40% of 575 = 230
 
-        buttonPanel.add(okButton);
-        buttonPanel.add(cancelButton);
-        add(buttonPanel, BorderLayout.SOUTH);
+		int remainingWidth = DIALOG_WIDTH - CHECKBOX_COL_WIDTH;
+		int nsWidth = (int) (remainingWidth * 0.2);
+		int podWidth = (int) (remainingWidth * 0.4);
+		int containerWidth = remainingWidth - nsWidth - podWidth; // Remainder to avoid rounding errors gaps
 
-        setSize(600, 400);
-        setLocationRelativeTo(parent);
+		table.getColumnModel().getColumn(1).setPreferredWidth(nsWidth);
+		table.getColumnModel().getColumn(2).setPreferredWidth(podWidth);
+		table.getColumnModel().getColumn(3).setPreferredWidth(containerWidth);
 
-        // Escape key to close
-        getRootPane().registerKeyboardAction(e -> dispose(),
-                KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0),
-                JComponent.WHEN_IN_FOCUSED_WINDOW);
-    }
+		K8sPodSelectionFilterPanel filterPanel = new K8sPodSelectionFilterPanel(table, sorter);
+		FilteredTablePanel filteredTablePanel = new FilteredTablePanel(table, filterPanel);
 
-    private void collectSelected() {
-        for (int i = 0; i < tableModel.getRowCount(); i++) {
-            if ((Boolean) tableModel.getValueAt(i, 0)) {
-                selectedContainers.add(new SelectedContainer(
-                        (String) tableModel.getValueAt(i, 1),
-                        (String) tableModel.getValueAt(i, 2),
-                        (String) tableModel.getValueAt(i, 3)));
-            }
-        }
-    }
+		add(filteredTablePanel, BorderLayout.CENTER);
 
-    public List<SelectedContainer> getSelectedContainers() {
-        return confirmed ? selectedContainers : new ArrayList<>();
-    }
+		JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		JButton okButton = new JButton("Import Selected");
+		okButton.addActionListener(e -> {
+			collectSelected();
+			LspLogger.info("User confirmed selection of " + selectedContainers.size() + " containers from selection dialog.");
+			confirmed = true;
+			dispose();
+		});
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.addActionListener(e -> dispose());
 
-    public static class SelectedContainer {
-        public final String namespace;
-        public final String pod;
-        public final String container;
+		buttonPanel.add(okButton);
+		buttonPanel.add(cancelButton);
+		add(buttonPanel, BorderLayout.SOUTH);
 
-        public SelectedContainer(String namespace, String pod, String container) {
-            this.namespace = namespace;
-            this.pod = pod;
-            this.container = container;
-        }
-    }
+		setSize(DIALOG_WIDTH, DIALOG_HEIGHT);
+		setLocationRelativeTo(parent);
+
+		// Escape key to close
+		getRootPane().registerKeyboardAction(e -> dispose(), KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE, 0),
+				JComponent.WHEN_IN_FOCUSED_WINDOW);
+	}
+
+	private void collectSelected() {
+		for (int i = 0; i < tableModel.getRowCount(); i++) {
+			if ((Boolean) tableModel.getValueAt(i, 0)) {
+				selectedContainers.add(new SelectedContainer((String) tableModel.getValueAt(i, 1), (String) tableModel.getValueAt(i, 2),
+						(String) tableModel.getValueAt(i, 3)));
+			}
+		}
+	}
+
+	public List<SelectedContainer> getSelectedContainers() {
+		return confirmed ? selectedContainers : new ArrayList<>();
+	}
+
+	public static class SelectedContainer {
+		public final String namespace;
+		public final String pod;
+		public final String container;
+
+		public SelectedContainer(String namespace, String pod, String container) {
+			this.namespace = namespace;
+			this.pod = pod;
+			this.container = container;
+		}
+	}
 }
